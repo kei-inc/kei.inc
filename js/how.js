f7b984913b98e1101010bdef99dc76ddf85401cd
milesbrown24 <downtownmilesbrown@gmail.com>
2024-07-23 18:08:55 -0700

    updated How page, couldn't do image as background for section 8 and 9

--- js/how.js
+++ js/how.js
@@ -1,14 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const textContainers = document.querySelectorAll(".text-container");
+  const sections = document.querySelectorAll(".section");
+  const header = document.querySelector(".sticky-header");
+  const logo = header.querySelector(".logo img");
+  const navLinks = header.querySelectorAll("nav a");
+  const currentNavLink = document.querySelector("nav .current-page");
   const triggerPosition = 0.5; // Adjust this value to control the appearance position
 
   const handleScroll = () => {
-    textContainers.forEach(container => {
-      const rect = container.getBoundingClientRect();
+    sections.forEach((section) => {
+      const rect = section.getBoundingClientRect();
       const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
-      
-      if (rect.top <= windowHeight * triggerPosition) {
-        container.classList.add("fadein-visible");
+
+      if (rect.top <= windowHeight * triggerPosition && rect.bottom > windowHeight * triggerPosition) {
+        console.log(`Section in view: ${section.dataset.bgColor}, ${section.dataset.textColor}, ${section.dataset.navColor}, ${section.dataset.logoColor}`);
+
+        document.body.style.backgroundColor = section.dataset.bgColor;
+        section.style.color = section.dataset.textColor;
+        header.style.backgroundColor = section.dataset.bgColor;
+        logo.style.filter = `invert(${section.dataset.logoColor === 'white' ? 1 : 0})`;
+
+        navLinks.forEach(link => {
+          link.style.color = section.dataset.navColor;
+        });
+
+        currentNavLink.style.color = section.dataset.navColor; // Set the current-page link color
+        section.classList.add("fadein-visible");
+
+        // Change the color of all text within the section
+        const texts = section.querySelectorAll('p');
+        texts.forEach(text => {
+          text.style.color = section.dataset.textColor;
+        });
+
+      } else {
+        section.classList.remove("fadein-visible");
       }
     });
   };
