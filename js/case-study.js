f33c6587146b2cbe1aa8cd1d7b13456f251ad313
paul-cloutier <paul@kei.inc>
2024-07-25 20:59:15 -0500

    updating for carousel on vibrant planet

--- js/case-study.js
+++ js/case-study.js
@@ -33,5 +33,39 @@ document.addEventListener("DOMContentLoaded", () => {
         });
       });
     }
+    
+    
+    /* --- vibrant planet carousel --- */
+    const imageSlides = document.querySelectorAll('.image-container .slide');
+    const textSlides = document.querySelectorAll('.text-content .slide');
+    let currentSlide = 0;
+    
+    const prevArrow = document.getElementById('prevArrow');
+    const nextArrow = document.getElementById('nextArrow');
+    
+    function updateSlide() {
+      imageSlides.forEach((slide, index) => {
+        if (index === currentSlide) {
+          slide.classList.add('active');
+          textSlides[index].classList.add('active');
+        } else {
+          slide.classList.remove('active');
+          textSlides[index].classList.remove('active');
+        }
+      });
+    }
+    
+    function nextSlide() {
+      currentSlide = (currentSlide + 1) % imageSlides.length;
+      updateSlide();
+    }
+    
+    function prevSlide() {
+      currentSlide = (currentSlide - 1 + imageSlides.length) % imageSlides.length;
+      updateSlide();
+    }
+    
+    nextArrow.addEventListener('click', nextSlide);
+    prevArrow.addEventListener('click', prevSlide);
   });
   
